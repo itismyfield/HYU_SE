01fd673caadf4a1e4de36732ba78ed1a466b4b05
CCman <sayggk@gmail.com>
2016-06-17 15:04:50 +0900

    fri 15:04

--- app/src/main/java/hanyang/hyu_se/MainFragment.java
+++ app/src/main/java/hanyang/hyu_se/MainFragment.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
+//import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
